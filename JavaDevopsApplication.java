504e9c68e70878bbf70170609ea904e1256c4623
vishal <vs7685@gmail.com>
2026-02-20 20:15:29 +0530

    Add files via upload

---
+++ JavaDevopsApplication.java
@@ -0,0 +1,13 @@
+package org.example.javadevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsApplication.class, args);
+    }
+
+}
